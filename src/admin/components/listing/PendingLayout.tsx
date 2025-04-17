a523a5516a92d4e6db82da1a7ce06ac368d3e378
ducothepaladin <lin303393@gmail.com>
2025-04-17 18:35:07 +0630

    feat: pending post listing

--- src/admin/components/listing/PendingLayout.tsx
+++ src/admin/components/listing/PendingLayout.tsx
@@ -1,11 +1,12 @@
 import AdminPostVerifyForm from "./AdminPostVerifyForm"
+import PendingPostLists from "./PendingPostLists"
 
 
 export default function PendingLayout() {
   return (
     <div className="grid grid-cols-2 gap-4 mt-5 px-5">
         <AdminPostVerifyForm />
-        <div>Hello</div>
+        <PendingPostLists />
     </div>
   )
 }
