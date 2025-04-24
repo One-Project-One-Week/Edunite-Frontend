e91a4e2628600559119930b0bd8c89eaee2eabb8
ducothepaladin <lin303393@gmail.com>
2025-04-24 20:22:28 +0630

    feat: user service and course service added

---
+++ src/services/userServices.ts
@@ -0,0 +1,55 @@
+import API from "@/api/apiConfig";
+
+export const getUserById = async (id: string) => {
+    try {
+        const response = await API.get(`/users/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getAllUsers = async () => {
+    try {
+        const response = await API.get("/users");
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getAllTeachers = async () => {
+    try {
+        const response = await API.get("/users/teachers");
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getAllStudents = async () => {
+    try {
+        const response = await API.get("/users/students");
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getCoursesByUserId = async (userId: string) => {
+    try {
+        const response = await API.get(`/users/${userId}/courses`);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error fetching courses by user ID: ${error}`);
+    }
+};
+
+export const createCourseByUserId = async (userId: string, courseData: any) => {
+    try {
+        const response = await API.post(`/users/${userId}/courses`, courseData);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Error creating course for user ID: ${error}`);
+    }
+};
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,3 +1,5 @@
+import { Course } from "./Course";
+
 export type User = {
         id: number,
         name: string,
@@ -5,3 +7,32 @@ export type User = {
         phone_number: number,
         role: "Admin" | "User" | "Seller",
 };
+
+export type BaseUser = {
+        id: number,
+        name: string,
+        email: string,
+        phone_number: number,
+        role: "Admin" | "Stundent" | "Teacher"
+}
+
+export type ProfessionSubject = {
+        name: string,
+}
+
+export type BasicSubject = {
+        name: string,
+        grade: string,
+}
+
+
+export type Teacher = BaseUser & {
+        created_courses: Course[],
+        subjects_type: "profession" | "basic"
+        subjects: ProfessionSubject[] | BasicSubject[],
+}
+
+export type Stundent = BaseUser & {
+        enrolled_courses: Course[],
+        grade: string,
+}
