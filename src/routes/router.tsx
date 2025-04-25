3d37b524db23bc4e47c9407006c71ad343f438c0
Aung-IO <apkdust@gmail.com>
2025-04-25 20:34:42 +0630

    update admin dashboard UI and add page transition

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,18 +3,25 @@ import authRoute from "./auth/AuthRoute";
 import publicRoute from "./public/PublicRoute";
 import adminRoute from "./admin/adminRoute";
 import dashboardRoute from "./dashboard/DashboardRoute";
+import { PageWrapper } from "@/components/PageWrapper";
 
-
+function wrapRoutesWithAnimation(routes : any) {
+    return routes.map((route : any) => ({
+      ...route,
+      element: <PageWrapper>{route.element}</PageWrapper>,
+      children: route.children ? wrapRoutesWithAnimation(route.children) : undefined,
+    }));
+  }
 
 const router = createBrowserRouter([
     {
         errorElement: <div>404</div>
     },
 
-    ...authRoute,
-    ...adminRoute,
-    ...publicRoute,
-    ...dashboardRoute,
+    ...wrapRoutesWithAnimation(authRoute),
+    ...wrapRoutesWithAnimation(adminRoute),
+    ...wrapRoutesWithAnimation(publicRoute),
+    ...wrapRoutesWithAnimation(dashboardRoute),
 ]);
 
 export default router;
