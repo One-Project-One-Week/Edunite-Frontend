0e12e23dec010423826a30ebd48ef5fcf80d47d0
ducothepaladin <lin303393@gmail.com>
2025-04-27 00:10:35 +0630

    type fix

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,21 +1,20 @@
 import {create} from 'zustand';
 import { BaseUser, Stundent, Teacher } from '@/types/User';
 type UserStore = {
-    user: Stundent | Teacher | BaseUser,
+    user: any,
     setUser: (user: Stundent | Teacher | BaseUser) => void,
     clearUser: () => void,
 }
 
 const useUserStore = create<UserStore>((set) => ({
-    user: JSON.parse(localStorage.getItem("User") || '{}') as Stundent | Teacher | BaseUser,
-    setUser: (user: Stundent | Teacher | BaseUser) => {
-        set({ user})
+    user: JSON.parse(localStorage.getItem("User") || '{}'),
+    setUser: (user: any) => {
+        set({ user });
     },
     clearUser: () => {
-        set({ user: {} as Stundent | Teacher | BaseUser})
+        set({ user: {} as any });
     }
-    
-}))
+}));
 
 
 export default useUserStore;
